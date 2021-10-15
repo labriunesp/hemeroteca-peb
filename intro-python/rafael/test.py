5c970152b5921a847745fde630c366808849e5ab
Thiago Vicino <thiago.vicino@unesp.br>
2021-10-14 23:44:00 -0300

    mudanças avulsas

--- intro-python/rafael/test.py
+++ intro-python/rafael/test.py
@@ -1,3 +1,14 @@
+import os #biblioteca nativa
+import sh # biblioteca de terceiro
+
+# serve para apontar o tipo de dado/string
+type('Bom dia')
+
+# exit() é usado para sair da pasta
+
+#funções built-in
 print ('Hello world')
 
 print ('Bom dia')
+
+print(os.listdir())
