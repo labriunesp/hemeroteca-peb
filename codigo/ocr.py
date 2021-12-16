fef4e4f623921ed492569da07d05c54e7cb6d9b6
Thiago Vicino <thiago.vicino@unesp.br>
2021-12-16 19:03:41 -0300

    Inicio da realizaçao de OCR dos arquivos da hemeroteca

--- codigo/ocr.py
+++ codigo/ocr.py
@@ -1,1 +1,17 @@
-import 
+import os
+import ocrmypdf
+
+def fazer_ocr():
+    origem = '/home/lantri_thiagofernandes/codigo/hemeroteca-peb/exemplos/tif'
+    destino = '/home/lantri_thiagofernandes/codigo/hemeroteca-peb/exemplos/tif/pdf-pesquisavel'
+    for raiz, dirs, files in os.walk(origem):
+        for file in files:
+            print(f'{file[:-4]}.pdf')
+            #ocrmypdf.ocr(f'{origem}/{file}',f'{destino}/{file[:-4]}.pdf',deskew=True)
+        
+
+def main():
+    fazer_ocr()
+
+if __name__=='__main__':
+    main()
