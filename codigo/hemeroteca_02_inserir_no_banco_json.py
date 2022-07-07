183d1e19915237645e0581927129149878da1d89
Thiago Vicino <thiago.vicino@unesp.br>
2022-07-07 17:57:54 -0300

    extraindo metadados a partir do nome do arquivo

--- codigo/hemeroteca_02_inserir_no_banco_json.py
+++ codigo/hemeroteca_02_inserir_no_banco_json.py
@@ -1,11 +1,15 @@
 from tinydb import TinyDB,Query
 import os
+from time import sleep
+import re
 #juntar tifs
 
-def inserir_json():
+def gerar_metadados():
     origem = '/media/hdvm08/bd/002/997/001/tif3/'
     for raiz, dirs, arqs in sorted(os.walk(origem)):
+        sleep(4)
         for index,arq in enumerate(arqs, start=1):
+            print(index, arq)
             arq_dir_completo = os.path.join(raiz,arq)
             codigo_bd = '/002/997/001'
             lista_caminho = arq_dir_completo.split('/')
@@ -39,7 +43,7 @@ def inserir_json():
                     jornal = "Gazeta Mercantil"
                 elif jornal_sigla == "VLR":
                     jornal = "Valor Econômico"
-                elif jornal_sigla == "JB":
+                elif jornal_sigla == "JB-":
                     jornal = "Jornal do Brasil"
                 elif jornal_sigla == "SBP":
                     jornal = "Jornal da Ciência"
@@ -47,59 +51,53 @@ def inserir_json():
                     jornal = "Jornal do Comércio"
             except:
                 jornal = "NA"
-            #['', 'media', 'hdvm08', 'bd', '002', '997', '001', 'tif3', '12-brasil-politica_exterior', '1987-03-13-ESP-Brasil_ignora_reuniao_nos_EUA-p01.tif']
+            #['1987-03-13-ESP-Brasil_ignora_reuniao_nos_EUA-p01.tif']
             try:
-                #CONTINUAR A PARTIR DAQUI(06/07/2022)
-                titulo_noticia = lista_caminho[-1]
+                titulo_noticia = lista_caminho[-1].split("-")[4].replace("_"," ")
+                titulo_noticia = re.sub('([a-z,A-Z])', lambda x: x.groups()[0].upper(),titulo_noticia,1).strip()
+
+                print(titulo_noticia)
             except:
                 titulo_noticia = "NA"
-
-    '''
-        titulo_noticia = lista_nome_arquivo[4:]
-        titulo_noticia = " ".join(titulo_noticia)
-        titulo_noticia = titulo_noticia[0:-4].replace('_', " ").replace("  "," ").replace('(1)',"")
-        titulo_noticia = re.sub('([a-z,A-Z])', lambda x: x.groups()[0].upper(),titulo_noticia,1).strip()
-        if 'page0' in titulo_noticia:
-            titulo_noticia = titulo_noticia[:-8].strip()
-        elif '_p0' in titulo_noticia:
-            titulo_noticia = titulo_noticia[:-4].strip()
-    except:
-        titulo_noticia = 'NA'
-        print(f'Data: {data}') 
-    print(f'Nome jornal: {sigla_jornal}')
-    #print(f'Título: {titulo_noticia}')
-    dir_bd = '/media/hdvm08/bd/002/997/001/json'
-    db = TinyDB(f'{dir_bd}/teste12.json', indent = 4, ensure_ascii=False)
-    buscar = Query()
-    verifica_db = db.contains((buscar.titulo_noticia==titulo_noticia)&(buscar.data==data)&(buscar.nome_arquivo_tif==nome_arquivo_tif))
-    if not verifica_db:
-        print('Não está na base')
-        db.insert({
-            'tema':tema,
-            'data':data,
-            'jornal':nome_jornal,
-            'jornal_sigla': sigla_jornal,
-            'titulo_noticia':titulo_noticia,
-            'nome_arquivo_tif': nome_arquivo_tif,
-            'nome_arquivo-pdf': "NA",
-            'quant_pages': len(nome_arquivo_tif),
-            'verifica_ocr': "NA",
-            'paragrafos': "NA",
-            'autoria': "NA",
-            'dir_bd': dir_bd,
-            'dir_arquivo': origem_caminho_tif,
-            'codigo_bd': codigo_bd,
-            'extra_01': "NA",
-            'extra_02': "NA"
-        })
-        #fazer_ocr(origem_caminho_tif)
-    else:
-        print('JÁ ESTÁ NA BASE')
-        pass'''
+            nome_arquivo_tif = []
+            nome_arquivo_tif.append(arq)
+            if 
+            arq_tif = arq.split("-")[-2]
+            if arq_tif == ""
+    
+def inserir_bd():
+            dir_bd = '/media/hdvm08/bd/002/997/001/json'
+            db = TinyDB(f'{dir_bd}/teste12.json', indent = 4, ensure_ascii=False)
+            buscar = Query()
+            verifica_db = db.contains((buscar.titulo_noticia==titulo_noticia)&(buscar.data==data)&(buscar.nome_arquivo_tif==nome_arquivo_tif))
+            if not verifica_db:
+                print('Não está na base')
+                db.insert({
+                    'tema':tema,
+                    'data':data,
+                    'jornal':jornal,
+                    'jornal_sigla': jornal_sigla,
+                    'titulo_noticia':titulo_noticia,
+                    'nome_arquivo_tif': nome_arquivo_tif,
+                    'nome_arquivo-pdf': "NA",
+                    'quant_pages': len(nome_arquivo_tif),
+                    'verifica_ocr': "NA",
+                    'paragrafos': "NA",
+                    'autoria': "NA",
+                    'dir_bd': dir_bd,
+                    'dir_arquivo': origem_caminho_tif,
+                    'codigo_bd': codigo_bd,
+                    'extra_01': "NA",
+                    'extra_02': "NA"
+                })
+                #fazer_ocr(origem_caminho_tif)
+            else:
+                print('JÁ ESTÁ NA BASE')
+                pass
 
 
 def main():
-    inserir_json()
+    gerar_metadados()
 
 if __name__ == '__main__':
     main()
