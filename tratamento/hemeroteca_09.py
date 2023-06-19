cbf73d7c73e2ced21f1b302c3a290f2459db64e2
João Paulo Cintra Silva <cintrasilvajoaopaulo1@gmail.com>
2023-06-19 18:51:11 -0300

    verificação de similariedade pelo nome do arquivo pdf

--- tratamento/hemeroteca_09.py
+++ tratamento/hemeroteca_09.py
@@ -13,21 +13,22 @@ def encontrar_titulos_similares(lista, limite):
         for j in range(i + 1, len(lista)):
             similaridade = fuzz.token_set_ratio(lista[i], lista[j])
             if similaridade >= limite:
-                print(similaridade)
-                print(lista[i])
-                print(lista[j])
-                print("###")
+                with open("verificar.txt","a") as f:    
+                    f.write(f"{str(similaridade)}\n")
+                    f.write(f"{str(lista[i])}\n")
+                    f.write(f"{str(lista[j])}\n")
+                    f.write(f"{str('###')}\n")
                 titulos_similares.append((lista[i], lista[j], similaridade))
     return titulos_similares
 
 
 def main():
 
-    dataset = pd.read_json(f'/home/lantri_rafael/codigo/hemeroteca-peb/json/METADADOS_FINAL_copy.json', orient="columns")
+    dataset = pd.read_json(f'/home/lantri_joaosilva/codigo/hemeroteca-peb/json/METADADOS_FINAL_copy_ajustado.json', orient="columns")
     dataset = pd.json_normalize(dataset["METADADOS_FINAL.json"])
     #print(dataset)
     # Filtrar os títulos de notícias com base na similaridade
-    lista_titulos = dataset['titulo_noticia'].tolist() 
+    lista_titulos = dataset['nome_arquivo_pdf'].tolist() 
 
     titulos_similares = encontrar_titulos_similares(lista_titulos, 85)
 
